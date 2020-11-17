c2ff04c29b8359268a00d8fafadd6ab2a39b0f37
prabhu-sunderaraman <prabhu.bits@gmail.com>
2020-11-17 10:07:08 +0530

    Day02

---
+++ Day02/arrays.js
@@ -0,0 +1,7 @@
+let arr = [];
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length);
+for(let i=0; i<numbers.length; i++) {
+    let item = numbers[i];
+    console.log(item);
+}
---
+++ Day02/types.js
@@ -0,0 +1,16 @@
+//Java is a statically typed language
+//JS is a dynamically typed language
+//semi-colon is optional
+
+let i = 10;
+i = 10.34;
+let s = "hello";
+s = 'hi';
+let b = false
+console.log(typeof(i), typeof(s), typeof(b));
+let a; //undefined
+if (a == undefined) {
+    console.log("a is undefined");
+} else {
+    console.log(a);
+}
