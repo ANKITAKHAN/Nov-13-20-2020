f47cba92d853331da851fcd5614b8b6dafaae0f4
prabhu-sunderaraman <prabhu.bits@gmail.com>
2020-11-17 14:37:55 +0530

    Day02

---
+++ Day02/objects.js
@@ -0,0 +1,28 @@
+//let arr = new Array(1, 2, 3, 4, 5);
+let arr = [1, 2, 3, 4, 5];
+
+//let str = new String("something");
+let str = "something";
+
+//let num = new Number(10);
+let num = 10;
+
+//let obj = new Object();
+let obj = {};
+//JSON or JavaScript Object Notation
+let car = {
+    model: "BMW",
+    year: 2020,
+    drive(speed) {
+        console.log(`Driving ${this.model} at a speed of ${speed} km/h`);
+    }
+};
+console.log(car);
+car.drive(100);
+
+let person = {
+    name: "Sam", age: 12, hobbies: ["TV", "Eat", "Sleep"]
+};
+
+
+
