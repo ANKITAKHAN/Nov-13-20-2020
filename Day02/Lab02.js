caf1ba9cbeb514a7b8eed55dc7420877e417f4a9
prabhu-sunderaraman <prabhu.bits@gmail.com>
2020-11-17 11:34:07 +0530

    Day02

---
+++ Day02/Lab02.js
@@ -0,0 +1,32 @@
+function add() {
+    let sum = 0;
+    for(let i = 0; i < arguments.length; i++) {
+        let number = arguments[i];
+        if (typeof(number) === "number") {
+            sum += number;
+        }
+    }
+    console.log(`Sum is ${sum}`);
+}
+
+function greatest() {
+    let max = arguments.length > 0 ? arguments[0] : 0;
+    for(let i = 0; i < arguments.length; i++) {
+        let number = arguments[i];
+        if (typeof(number) === "number") {
+            if (number > max) {
+                max = number;
+            }
+        }
+    }
+    console.log(`Max is ${max}`);
+}
+
+
+add(1,5,34,324,324,235,2353);
+add(21,65);
+add(1,55,83);
+
+greatest(1,5,34,324,324,235,2353);
+greatest(21,65);
+greatest(1,55,83,9,667);
